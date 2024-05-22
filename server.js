8c44ad2cc14b842122910df28238963250f4e900
cesar.rodriguezg <cesar.rodriguezg@viewnext.com>
2024-05-22 17:28:16 +0200

    Creación login.html

--- server.js
+++ server.js
@@ -2,7 +2,7 @@ const express = require('express');
 const path = require('path');
 const app = express();
 
-const port = 3030;
+const port = 3000;
 
 app.use(express.json())
 app.use(express.static(path.join(__dirname, 'public')))
@@ -10,6 +10,11 @@ app.use(express.static(path.join(__dirname, 'public')))
 app.route('/')
   .get((_req, res) => {
     return res.sendFile(path.join(__dirname, 'public', 'index.html'))
+})
+
+app.route('/login')
+  .get((_req, res) => {
+    return res.sendFile(path.join(__dirname, 'public', 'login.html'))
 })
 
 
